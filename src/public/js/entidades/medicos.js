f91be28d8af09a67eba4f17bc07ee4506d303cdf
GREYES <analistaprogramador@clinicasanpedro.com>
2023-10-11 14:25:50 -0500

    Formulario de cliente

--- src/public/js/entidades/medicos.js
+++ src/public/js/entidades/medicos.js
@@ -1,20 +1,30 @@
 
 
-function guardarMedico(){
+function guardarMedico() {
     validarFormulario('medcel,medTelfij,med_correo,meddir,med_firma');
 }
 
-/*function bloquearlabel(){
-    const docideSelect = document.getElementById('docide');
-    const nundocInput = document.getElementById('nundoc');
 
-    docideSelect.addEventListener('change', function () {
-        // Habilita o deshabilita el campo de número según la selección
-        if (this.value === '1','2','3','4') {
-            nundocInput.disabled = false;
-        } else {
-            nundocInput.disabled = true;
-            nundocInput.value = ''; // Limpia el campo si está deshabilitado
-        }
-    });
-}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
