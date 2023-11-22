294ee7e7a21f6dfecc71e5d30734b14f7cd72b54
Dante Velasquez <analistadesarrollador@clinicasanpedro.com>
2023-11-22 18:52:43 -0500

    Últimos cambios

--- src/public/js/historiaclinica/examenes/pbsignosvitales.js
+++ src/public/js/historiaclinica/examenes/pbsignosvitales.js
@@ -33,7 +33,7 @@
     let recomendacion3 = document.getElementById('recomendacion3');
     let control3 = document.getElementById('control3');
     $.ajax({
-        url: '/resultfichamusculoesqueletica',
+        url: '/resultsignosvitales',
         method: "GET",
         data: {
             cita_id: cita_id,
